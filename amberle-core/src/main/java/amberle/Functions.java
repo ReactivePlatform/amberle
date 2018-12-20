f72fc25ce801a5af4ec0553d7626502f0a33c7a0
hepin1989 <hepin1989@gmail.com>
2018-12-21 01:49:50 +0800

    try to introduce functions.

---
+++ amberle-core/src/main/java/amberle/Functions.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2018 amberle-core
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package amberle;
+
+import java.util.function.Function;
+
+public class Functions {
+  private Functions() {}
+
+  private static final Function<Object, Object> CONSTANT_IDENTITY_FUNCTION = input -> input;
+
+  @SuppressWarnings("unchecked")
+  public static final <T> Function<T, T> identity() {
+    return (Function<T, T>) CONSTANT_IDENTITY_FUNCTION;
+  }
+}
